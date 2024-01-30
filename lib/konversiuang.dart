81950e09ecdddc081351d2e8da153b6e7ce0df87
Raditya H <radityahd39@gmail.com>
2024-01-31 01:14:29 +0700

    Add "convert to currency" feature

---
+++ lib/konversiuang.dart
@@ -0,0 +1,11 @@
+import 'package:intl/intl.dart';
+
+class CurrencyFormat {
+  static String convertToIdr(dynamic number) {
+    NumberFormat currencyFormatter = NumberFormat.currency(
+      locale: 'id',
+      symbol: 'Rp ',
+    );
+    return currencyFormatter.format(number);
+  }
+}
